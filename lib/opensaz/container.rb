43d3eb3fd70b900e44a1c0b8e98a145357d6883c
CY's laptop <yang.cong@moodys.com>
2016-07-30 21:33:45 +0800

    add to_hash function for Opensaz::Container class and add minitest case

--- lib/opensaz/container.rb
+++ lib/opensaz/container.rb
@@ -4,29 +4,42 @@ module Opensaz
   class Container
     def initialize(destination)
       @index_file = File.join(destination, "_index.htm")
+      @page = nil
       @packages = nil
     end
 
     def to_hash
-      raise "file doesn't exist #{@index_file}" unless File.exist?(@index_file)
-      @page = Nokogiri::HTML(open(@index_file))
-      keys = get_keys
+      @page ||= Nokogiri::HTML(load_page)
+      keys = get_thead
       ary = []
-      @page.css('tbody tr').each do |tr|
-        i = 0
-        a_hash = {}
-        tr.css('td').each {|td| a_hash[keys[i]] = td.text; i += 1 }
-        puts a_hash
+      @page.css('tbody tr').each do |x|
+        values = get_tbody_tr(x)
+        tmp = (0...keys.size).map{ |i| [keys[i], values[i]] }.to_h 
+        ary.push(tmp)
       end
+      ary
     end
 
     private
 
-    def get_keys
-      @page.css('thead tr th').map{|x| x.text == "#" ? :id : x.text.downcase.to_sym }
+    def load_page
+      raise "file doesn't exist #{@index_file}" unless File.exist?(@index_file)
+      @page = Nokogiri::HTML(open(@index_file))
     end
 
-    def seperate_c_s_m
+    def get_thead
+      [:c, :s, :m] + 
+      @page.css('thead tr th')[1..-1]
+        .map {|x| x.text == "#" ? :id : x.text.downcase.to_sym}
+    end
+
+    def get_tbody_tr(tr_node)
+      tds = tr_node.css('td')
+      seperate_c_s_m(tds[0]) + tds[1..-1].map{|x| x.text }
+    end
+
+    def seperate_c_s_m(a_node)
+      a_node.css('a').map{|a| a["href"] }
     end
   end
 end
---
+++ test/container_test.rb
@@ -0,0 +1,49 @@
+require_relative 'test_helper.rb'
+
+class ContainerTest < Minitest::Test
+  def setup
+    @container = Opensaz::Container.new("dest")
+  end
+
+  def test_to_hash
+    str1 =<<EOF
+<thead>
+<tr>
+  <th>&nbsp;</th>
+  <th>#</th>
+  <th>url</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+  <td>
+    <a href="url_1">C</a>
+    <a href="url_2">S</a>
+    <a href="url_3">M</a>
+  </td>
+  <td>1</td>
+  <td>/entity/id</td>
+</tr>
+<tr>
+  <td>
+    <a href="url_4">C</a>
+    <a href="url_5">S</a>
+    <a href="url_6">M</a>
+  </td>
+  <td>2</td>
+  <td>/entity/name</td>
+</tr>
+</tbody>
+EOF
+    @container.stub :load_page, str1 do 
+      ary = @container.to_hash
+      assert_equal 2, ary.size
+      assert_equal "2", ary[1][:id]
+      assert_equal "1", ary[0][:id]
+      assert_equal "/entity/name", ary[1][:url]
+      assert_equal "url_1", ary[0][:c]
+      assert_equal "url_2", ary[0][:s]
+      assert_equal "url_3", ary[0][:m]
+    end
+  end
+end
